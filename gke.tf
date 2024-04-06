3e93a755f46cb148335611827fdcc1a4c70eae14
assetrakhmet <rakhmetovasset@gmail.com>
2024-04-06 12:25:45 -0500

    disk gb

--- gke.tf
+++ gke.tf
@@ -56,7 +56,8 @@ resource "google_container_node_pool" "primary_nodes" {
     }
 
     # preemptible  = true
-    machine_type = "e2-medium"
+    machine_type = "n2-standars-2"
+    disk_size_gb = 10
     tags         = ["gke-node", "${var.project_id}-gke"]
     metadata = {
       disable-legacy-endpoints = "true"
