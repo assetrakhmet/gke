99cd6d9c95f9e6c53ebfbe9319532a712064b81d
assetrakhmet <142461440+assetrakhmet@users.noreply.github.com>
2024-04-06 11:45:42 -0500

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -41,6 +41,7 @@ resource "google_container_node_pool" "primary_nodes" {
   name       = google_container_cluster.primary.name
   location   = var.region
   cluster    = google_container_cluster.primary.name
+  disk_size  = 10
   
   version = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
   node_count = var.gke_num_nodes
